05ad430a9d3fe482afe52c261f389a0d4135edda
Júlio Mühlbauer <juliowernermm@gmail.com>
2025-07-12 16:05:12 -0300

    add chakra ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
-export const Route = createFileRoute('/')({
+import { Button } from "@chakra-ui/react";
+import { createFileRoute } from "@tanstack/react-router";
+export const Route = createFileRoute("/")({
   component: Home,
-})
+});
 
 function Home() {
   return (
     <div className="p-2">
       <h3>Welcome Home!!!</h3>
+
+      <Button>Click it</Button>
     </div>
-  )
+  );
 }
