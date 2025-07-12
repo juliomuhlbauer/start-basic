aa1553fbfe2bed48f536e1961dc9799a0adffd82
Júlio Mühlbauer <juliowernermm@gmail.com>
2025-07-12 17:26:28 -0300

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     tsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
-    tanstackStart({ customViteReactPlugin: true, target: "bun" }),
+    tanstackStart({ customViteReactPlugin: true, target: "netlify" }),
     viteReact(),
   ],
 });
